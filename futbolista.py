d5f573c5da4b1f6332fbba908ae3e11e9fd6e5b0
Paula Misas <pmisas@unal.edu.co>
2022-10-18 06:05:53 -0500

    :anchor:

--- futbolista.py
+++ futbolista.py
@@ -5,23 +5,15 @@ class Futbolista(Persona,Deportista):
     listaFutbolistas = []
 
     def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
-
         
         Persona.__init__(self,nombre,edad,altura,sexo)
         Deportista.__init__(self,añosPracticando)
-        self._golesMarcados = golesMarcados
-        self._tarjetasRojas = tarjetasRojas
-        self._piernaHabil = piernaHabil
+        self._golesMarcados =golesMarcados
+        self._tarjetasRojas =tarjetasRojas
+        self._piernaHabil =piernaHabil
         Futbolista.listaFutbolistas.append(self)
 
-    def getPiernaHabil(self):
-        return self._piernaHabil
 
-    def setPiernaHabil(self,piernaHabil):
-        self._piernaHabil = piernaHabil
-
-    def getTarjetasRojas(self):
-        return self._tarjetasRojas
 
     def getGolesMarcados(self):
         return self._golesMarcados
@@ -29,8 +21,19 @@ class Futbolista(Persona,Deportista):
     def setGolesMarcados(self,golesMarcados):
         self._golesMarcados = golesMarcados
 
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+
     def setTarjetasRojas(self,tarjetasRojas):
         self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self,piernaHabil):
+        self._piernaHabil = piernaHabil
+
+
 
 
     @classmethod
@@ -41,6 +44,6 @@ class Futbolista(Persona,Deportista):
         clc.listaFutbolistas = listaFutbolistas
 
 
-
     def __str__(self):
         return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
+
